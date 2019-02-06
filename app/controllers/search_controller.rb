278be8266516822e71f3ba5245a2798a2788804d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-06 09:46:38 -0700

    Adds to SearchController index method. Not passing test.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,26 @@
 class SearchController < ApplicationController
 
   def index
+    thing = params[:thing]
+
+    @conn = Faraday.new(url: "") do |faraday|
+      faraday.headers[""] = ""
+      faraday.apapter Faraday.default_adapter
+    end
+
+    response = @conn.get("")
+
+    @stations = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
+  # def index
+  # state = params[:state]
+  # @conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+  #   faraday.headers["X-API-KEY"] = "S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX"
+  #   faraday.adapter Faraday.default_adapter
+  # end
+  #
+  # response = @conn.get("/congress/v1/members/house/#{state}/current.json")
+
+  # @members = JSON.parse(response.body, symbolize_names: true)[:results]
 end
