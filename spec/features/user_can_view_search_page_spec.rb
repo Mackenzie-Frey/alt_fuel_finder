67e8c830cc1e5e62131a57d8a186b8173e7a54d2
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-06 10:00:05 -0700

    Adds fill_in for zip_code

--- spec/features/user_can_view_search_page_spec.rb
+++ spec/features/user_can_view_search_page_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'As a User visiting / I fill in the search form' do
   it 'See a list of stations and their info' do
     visit '/'
+    fill_in "q", with: "80203"
     click_on 'Locate'
 
     expect(current_path).to eq('/search')
